2c3e2073ccbcf4949c47a22b5b767bd43f02bcfd
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-03 01:30:42 +0530

    how take input in python

---
+++ input.py
@@ -0,0 +1,17 @@
+# # Taking Input from user 
+
+# name=input('Enter your name: ')
+# # name=int(name)❌❌❌❌❌ 
+# print(name)
+
+# # type casting ->converting one data type to another
+# age =input('Enter your age: ')
+# print(type(age))
+
+# rollno=int(input('Enter your rollno.'))
+# print(type(rollno))
+
+# sum of 2 given number
+num1=int(input('enter num1: '))
+num2=int(input('enter num2: '))
+print(num1+num2)
