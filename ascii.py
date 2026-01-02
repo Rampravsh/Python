a376a93d5eed23bb2c8ee1003be44bcd495404e7
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-03 01:13:42 +0530

    complete

--- ascii.py
+++ ascii.py
@@ -1,2 +1,12 @@
 char='a'
-print(ord(char))
+# ord() is a built in function for geting ascii value of char in python
+print(ord(char))  #97
+
+char='A'
+
+print(ord(char))  #65
+
+# chr() function is a built in function for geting char from ascii value in python
+
+ascii=90
+print(chr(ascii)) #Z
