054c6df1ffa5fd4c1d7f8d1e6fccf09e0eeb5381
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-03 01:03:23 +0530

    variables

--- variables.py
+++ variables.py
@@ -1,11 +1,33 @@
 # string 
 name="Riya"
+# print(type(name))
+
 # integer
 roll_number=17
+# print(type(roll_number))
+
 # floating number 
 percentage=99.9
+# print(type(percentage))
 
 # boolean 
 is_student = True
+# print(type(is_student))
 
-print(name,roll_number,percentage,is_student)
+# print(name,roll_number,percentage,is_student)
+percentage=94.43
+
+# print(name,roll_number,percentage,is_student)
+
+
+print('my name is '+name+" and my roll number is",roll_number)
+print("I scored ",percentage,"% in my final exams. I am a student is", is_student)
+# print expressions
+print('my percentage has changed to',percentage-1.3)
+
+# print with separator
+print(name,roll_number,percentage,is_student,sep='--')
+x=1
+y=2
+z=3
+print(x,y,z,sep='->')
