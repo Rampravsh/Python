449d3059dfc5f0bacebd71f06713a19c5644ecc7
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-04 04:47:33 +0530

    print elements of a list using for loop

--- forloop.py
+++ forloop.py
@@ -1,7 +1,13 @@
-for i in range(1,11,3):
+""" for i in range(1,11,3):
     print(i,'hello world')
 
 for _ in range(12):
-    print('hello')
+    print('hello') """
 
-    
+
+# print elements of a list using for loop
+
+list = [1,2,3,4,5,6,7,8,9]
+
+for i in list:
+    print(i)
