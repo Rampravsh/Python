4a1d434560e52fd0936ee1a5687346699bcbeb0c
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-04 00:56:35 +0530

    solve two problem on condition if else

---
+++ ifelse.py
@@ -0,0 +1,18 @@
+# Take integer input and tell if it is positive or negative
+""" input=input('Enter an integer: -')
+num=int(input)
+if num>0:
+    print('The number is positive')
+elif num<0:
+    print('The number is negative')
+else:
+    print('The number is zero') """
+
+# Take positive integer input and tell if it is even or odd
+
+input = int(input('Enter an number '))
+ 
+if input%2==0:
+    print('the number is even')
+else:
+    print('the number is odd')
