01c60df00c820f66d5197fafe78eb5905835c728
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-04 02:16:16 +0530

    Nasted If-Else

--- ifelse.py
+++ ifelse.py
@@ -68,9 +68,22 @@ n1=int(input("Enter n1 "))
 n2=int(input("Enter n2 "))
 n3=int(input("Enter n3 "))
 
-if n1>n2 and n1>n3:
+""" if n1>n2 and n1>n3:
     print('n1 is greatest number')
 elif n2>n1 and n2>n3:
     print('n2 is greatest number')
 else:
-    print('n3 is the greatest number')
+    print('n3 is the greatest number') """
+
+# Nasted If-Else
+
+if n1>n2:
+    if n1>n3:
+        print('n1 is greatest')
+    else:
+        print('n3 is greatest ')
+else:
+    if n2>n3:
+        print('n2 is greatest')
+    else:
+        print('n3 is greatest')
