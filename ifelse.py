ba85f823d09f0dcf4896f309ae67b2bb6c64e266
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-04 01:04:27 +0530

    third problem solve

--- ifelse.py
+++ ifelse.py
@@ -10,9 +10,23 @@ else:
 
 # Take positive integer input and tell if it is even or odd
 
-input = int(input('Enter an number '))
+""" input = int(input('Enter an number '))
  
 if input%2==0:
     print('the number is even')
 else:
-    print('the number is odd')
+    print('the number is odd') """
+
+# if cost price and selling price of an item is input through the keyboard, 
+# write a prgram to determine whether the seller has made profit or incurred loss
+# or no profit loss. Also determine how much profit he made or loss he incurred.
+
+cost =int(input('Enter the cost price of the product '))
+selling =int(input('Enter the selling price of the product '))
+
+if selling>cost:
+    print('you made profit on this product ',selling-cost)
+elif cost>selling:
+    print('you made loss on this product ',cost-selling)
+else:
+    print('no loss no profit on this product')
