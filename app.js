31c64e50f79b2fd57872980fd668a4606940d048
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-04 00:27:37 +0530

    dono hi push kar do kya hi hoga

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-let name ='rampravesh kumar'
-let percentage=92
-console.log('my name is:',name,percentage)
-console.log(name+percentage)
+// let name ='rampravesh kumar'
+// let percentage=92
+// console.log('my name is:',name,percentage)
+// console.log(name+percentage)
+
+// var singleNumber = function (nums) {
+//   let set = new Set();
+//   for (let i = 0; i < nums.length; i++) {
+//     if (set.has(nums[i])) {
+//       set.delete(nums[i]);
+//     } else {
+//       set.add(nums[i]);
+//     }
+//   }
+//   return set[0]
+// };
+// let nums = [2, 2, 1];
+// console.log(singleNumber(nums));
