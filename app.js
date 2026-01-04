7bc00eace1a01ae544c380266257605d057e9113
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-04 07:43:04 +0530

    pattern

--- app.js
+++ app.js
@@ -16,3 +16,11 @@
 // };
 // let nums = [2, 2, 1];
 // console.log(singleNumber(nums));
+let n = 10;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 1; j <=n; j++) {
+    row += j;
+  }
+  console.log(row);
+}
