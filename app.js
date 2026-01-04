ed144776e99acd019c2e184410f57507f74fa518
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-05 04:42:24 +0530

    some problem solve and start our day three

--- app.js
+++ app.js
@@ -16,11 +16,28 @@
 // };
 // let nums = [2, 2, 1];
 // console.log(singleNumber(nums));
-let n = 10;
-for (let i = 0; i < n; i++) {
-  let row = "";
-  for (let j = 1; j <=n; j++) {
-    row += j;
-  }
-  console.log(row);
-}
+// let n = 10;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 1; j <=n; j++) {
+//     row += j;
+//   }
+//   console.log(row);
+// }
+
+
+// var containsNearbyDuplicate = function(nums, k) {
+//     let map=new Map()
+//     for (let i=0;i<nums.length;i++){
+//         if(map.has(nums[i])){
+//             if(Math.abs(map.get(nums[i])-i)<=k){
+//                 return true
+//             }else{
+//                 map.set(nums[i],i)
+//             }
+//         }else{
+//             map.set(nums[i],i)
+//         }
+//     }
+//     return false
+// };
--- pattern.py
+++ pattern.py
@@ -4,8 +4,16 @@ for _ in range(n):
     print('*'*5) """
 
 
-n=int(input('enter the row no : '))
+""" n=int(input('enter the row no : '))
 for _ in range(n):
     for i in range(1,n+1):
         print(i,end='')
+    print() """
+
+n=int(input('enter number: '))
+for i in range(1,n+1):
+    for j in range(n-i,0,-1):
+        print(' ',end='')
+    for j in range(1,i*2):
+        print(j,end='')
     print()
