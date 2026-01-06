e0963a58aa16c56ca3fe689b392ede4c2e762f4f
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-06 07:56:16 +0530

    practice in dict

---
+++ dictionary/dictionary.py
@@ -0,0 +1,68 @@
+#creating a dictionary
+
+phones={
+    'john':93874,
+    'ria':279834,
+    'joy':3982
+}
+
+#printing the dict
+
+# print(phones)
+
+#checking type of dic
+# print(type(phones))
+
+#checking length of dict
+
+# print(len(phones))
+
+#access items of the dict
+
+# print(phones['john'])
+
+# print(phones.get('john'))
+
+# print(phones.keys())
+
+#update value in dict
+
+# phones['john']=398479823
+# print(phones)
+
+
+#add elements in dict
+# phones['pia']=283478273
+# print(phones)
+
+# more_phones={
+#     'kia':9834792
+# }
+
+# phones.update(more_phones)
+# print(phones)
+
+#remove elements from the dict
+
+# phones.pop('john')
+# print(phones)
+
+
+# phones.popitem()  #this will remove the last added item
+# print(phones)
+
+# phones.clear()  #this will empty the dict
+
+# print(phones)
+
+
+#pinting key of a dict 
+
+# for x in phones:
+#     print(x)
+
+#printing value of a dict 
+
+# for x in phones:
+#     print(phones[x])
+
