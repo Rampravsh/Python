a991a9fe617a8acf9fb12c8b40eedbd427013869
Rampravesh <rampraveshkr4545@gmail.com>
2026-01-07 05:53:51 +0530

    probelm solve and day 5 start

--- dictionary/probelm.py
+++ dictionary/probelm.py
@@ -12,3 +12,18 @@ for x in dict.values():
 # print(res)
 # print('The sum of dict values is: ',sum(dict.values()))
 
+# given a string and a number N, we need to mirror the characters from the N-th position up 
+# to the length of the string in alphabetical order . in mirror operation , we change 'a' to 
+# "z" ,"b"to "y",and so on.
+
+res=''
+string ="paradox"
+n=3
+for i in range(len(string)):
+    if i>=n-1:
+        acii=ord(string[i])
+        res+=chr(122-(acii-97))
+    else:
+        res+=string[i]
+
+print(res)
